4ba61211e499073f08dadbc1a3a7c462c85ec13b
vwh <vwhe@proton.me>
2025-06-18 06:44:40 +0300

    feat: added nvidia prime for the thinkpad host

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -12,6 +12,19 @@
   services.power-profiles-daemon.enable = true;
   services.fwupd.enable               = true;
 
+  hardware.nvidia.prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      # integrated
+      intelBusId = "PCI:0@2:0:0";
+
+      # dedicated
+      nvidiaBusId = "PCI:2@0:0:0";
+    };
+
   networking.hostName = hostname;
 
   system.stateVersion = stateVersion;
