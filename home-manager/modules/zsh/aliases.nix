45b86264dddd222451ed6ee4e52f45e85e9d1c86
vwh <vwhe@proton.me>
2025-06-20 13:44:37 +0300

    feat: update aliases

---
+++ home-manager/modules/zsh/aliases.nix
@@ -0,0 +1,27 @@
+{
+  programs.zsh.shellAliases =
+    let
+      flakeDir = "~/System";
+      editor = "code";
+      nixTemplates = "github:vwh/nixos-config/main?dir=nix-templates#";
+    in
+    {
+      nrs = "sudo nixos-rebuild switch --flake ${flakeDir}";
+      hms = "home-manager switch --flake ${flakeDir}";
+
+      nd = "nix develop --command zsh";
+      nfu = "nix flake update";
+      nfs = "nix flake show";
+      nfi = "nix flake init";
+
+      ll = "ls -l";
+      v = "${editor}";
+      se = "sudoedit";
+
+      "nfib" = "nix flake init -t '${nixTemplates}bun'";
+      "nfid" = "nix flake init -t '${nixTemplates}deno'";
+      "nfip" = "nix flake init -t '${nixTemplates}python-venv'";
+      "nfirs" = "nix flake init -t '${nixTemplates}rust-stable'";
+      "nfirn" = "nix flake init -t '${nixTemplates}rust-nightly'";
+  };
+}
--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -1,36 +1,14 @@
 { config, ... }:
 {
+  imports = [
+    ./aliases.nix
+  ];
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    # enableAutosuggestions = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
-    shellAliases =
-      let
-        flakeDir = "~/System";
-        editor = "code";
-        devShellPath = "github:vwh/.dotfiles?ref=main&dir=dev-environment";
-      in
-      {
-        rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
-        upd = "nix flake update ${flakeDir}";
-        upg = "sudo nixos-rebuild switch --upgrade --flake ${flakeDir}";
-
-        hms = "home-manager switch --flake ${flakeDir}";
-
-        conf = "${editor} ${flakeDir}/nixos/configuration.nix";
-        pkgs = "${editor} ${flakeDir}/nixos/packages.nix";
-
-        ll = "ls -l";
-        v = "${editor}";
-        se = "sudoedit";
-        ff = "fastfetch";
-
-        "bune" = "nix develop '${devShellPath}/bun' --command zsh";
-        "nodejse" = "nix develop '${devShellPath}/nodejs' --command zsh";
-      };
 
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
