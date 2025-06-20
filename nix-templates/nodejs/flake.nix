1ca08acccb14659354dd6b98bcaf640c7a11aa30
vwh <vwhe@proton.me>
2025-06-20 16:12:44 +0300

    feat: added nodejs on nix-templates

---
+++ nix-templates/nodejs/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NodeJS Javascript App";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        name = "bun-app";
+        buildInputs = with pkgs; [
+          nodejs
+        ];
+
+        shellHook = ''
+          if [ ! -f package.json ]; then
+            npm init -y
+          fi
+        '';
+      };
+    });
+}
+
