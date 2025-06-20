6b24dd3f4b3caef14491a79e098ccd357850f1ff
vwh <vwhe@proton.me>
2025-06-20 03:34:36 +0300

    feat: update nix.nix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,9 +1,14 @@
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 }
