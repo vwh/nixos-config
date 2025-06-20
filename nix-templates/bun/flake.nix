c936d6a01226e2a80777f54820996621e6cf7bd3
vwh <vwhe@proton.me>
2025-06-20 12:48:54 +0300

    feat: added bun on nix-templates & removed dev env folder

---
+++ nix-templates/bun/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Bun Javascript App";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        name = "bun-app";
+        buildInputs = with pkgs; [
+          bun
+        ];
+
+        shellHook = ''
+          if [ ! -f package.json ]; then
+            bun init
+          fi
+        '';
+      };
+    });
+}
+
