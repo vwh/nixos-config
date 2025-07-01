50d1b4c955331c98cdad4bfab7dec946b15f6498
vwh <vwhe@proton.me>
2025-07-01 13:55:01 +0300

    chore: comment the bat theme

--- home-manager/modules/bat.nix
+++ home-manager/modules/bat.nix
@@ -4,17 +4,17 @@
   programs.bat = {
     enable = true;
 
-    themes = {
-      dracula = {
-        src = pkgs.fetchFromGitHub {
-          owner = "Briles";
-          repo = "gruvbox";
-          rev = "75407cc80c51814d61beb1df07e380d6f58ad767";
-          sha256 = "186rhbljw80psf1l8hyj02ycz1wzxv4rxmbrqr8yvi30165drpay";
-        };
+    # themes = {
+    #   dracula = {
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "Briles";
+    #       repo = "gruvbox";
+    #       rev = "75407cc80c51814d61beb1df07e380d6f58ad767";
+    #       sha256 = "186rhbljw80psf1l8hyj02ycz1wzxv4rxmbrqr8yvi30165drpay";
+    #     };
 
-        file = "gruvbox (Dark) (Medium).sublime-color-scheme";
-      };
-    };
+    #     file = "gruvbox (Dark) (Medium).sublime-color-scheme";
+    #   };
+    # };
   };
 }
