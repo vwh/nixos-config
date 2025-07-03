daecc16a97a94baec8ff57d7099fc76ec004c819
vwh <vwhe@proton.me>
2025-07-03 14:57:39 +0300

    fix: bad electron env vars

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -6,7 +6,7 @@
     settings = {
       env = [
         # Hint Electron apps to use Wayland
-        "NIXOS_OZONE_WL,1"
+        "ELECTRON_OZONE_PLATFORM_HINT,auto"
         "XDG_CURRENT_DESKTOP,Hyprland"
         "XDG_SESSION_TYPE,wayland"
         "XDG_SESSION_DESKTOP,Hyprland"
--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -25,7 +25,7 @@ in
     GBM_BACKEND = "nvidia-drm"; # Graphics backend for Wayland
     __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # Use Nvidia driver for GLX
     WLR_NO_HARDWARE_CURSORS = "1"; # Fix for cursors on Wayland
-    NIXOS_OZONE_WL = "1"; # Wayland support for Electron apps
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
     __GL_GSYNC_ALLOWED = "1"; # Enable G-Sync if available
     __GL_VRR_ALLOWED = "1"; # Enable VRR (Variable Refresh Rate)
     WLR_DRM_NO_ATOMIC = "1"; # Fix for some issues with Hyprland
