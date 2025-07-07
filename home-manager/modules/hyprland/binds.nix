ccf97affb72d18a3ec6a773b60613933589b654f
vwh <vwhe@proton.me>
2025-07-07 22:34:20 +0300

    feat: better hyprland binds

--- home-manager/modules/hyprland/binds.nix
+++ home-manager/modules/hyprland/binds.nix
@@ -23,23 +23,30 @@ in
 
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "$mainMod SHIFT, Return, exec, $terminal"
-      "$mainMod SHIFT, C, killactive,"
-      "$mainMod SHIFT, Q, exit,"
+      "$mainMod SHIFT, Escape, exit,"
+      "$mainMod,       Home, exec, loginctl lock-session"
+
+      "$mainMod,       Return, exec, $terminal"
+      "$mainMod,       Q, killactive,"
       "$mainMod,       R, exec, $fileManager"
       "$mainMod,       F, togglefloating,"
       "$mainMod,       D, exec, $menu --show drun"
-      "$mainMod,       P, pin,"
       "$mainMod,       J, togglesplit,"
-      "$mainMod,       E, exec, bemoji -cn"
       "$mainMod,       V, exec, cliphist list | $menu --dmenu | cliphist decode | wl-copy"
-      "$mainMod,       B, exec, pkill -SIGUSR2 waybar"
-      "$mainMod SHIFT, B, exec, pkill -SIGUSR1 waybar"
-      "$mainMod,       L, exec, loginctl lock-session"
+
+      "$mainMod,       E, exec, bemoji -cn"
       "$mainMod,       P, exec, hyprpicker -an"
       "$mainMod,       N, exec, swaync-client -t"
-      ", Print, exec, WLR_RENDERER=egl grimblast --freeze --notify copysave area & pid=$!; ( sleep 10 && kill $pid ) &"
-      "$mainMod,       W, exec, ${booksScript}/bin/open_books"
+
+      "$mainMod,       W, exec, pkill -SIGUSR2 waybar"
+      "$mainMod SHIFT, W, exec, pkill -SIGUSR1 waybar"
+       # "$mainMod,       W, exec, ${booksScript}/bin/open_books"
+       
+      ", Print, exec, WLR_RENDERER=egl grimblast --freeze --notify copysave area & pid=$!; ( sleep 15 && kill $pid ) &"
+
+      # Volume control
+      "$mainMod, equal,  exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+      "$mainMod, minus, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
 
       # Moving focus
       "$mainMod, left, movefocus, l"
@@ -65,10 +72,6 @@ in
       "$mainMod, 3, workspace, 3"
       "$mainMod, 4, workspace, 4"
       "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
       "$mainMod, 0, workspace, 10"
 
       # Moving windows to workspaces
@@ -77,10 +80,6 @@ in
       "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
       "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
       "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
-      "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
-      "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
-      "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
-      "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
       "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
 
       # Scratchpad
