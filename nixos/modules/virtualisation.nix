ee47b4d4952497836451ab8e9860d0e6d00997e6
vwh <vwhe@proton.me>
2025-07-08 10:13:32 +0300

    feat: added virtualbox, libvirt for virtualisation

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -1,14 +1,39 @@
-# Virtualization (Docker) configuration.
+# Virtualisation ( docker, virtualbox, libvirt ) configuration.
 
-{ user, ... }:
+{ pkgs, user, ... }:
 
 {
+  boot.kernelModules = [
+    "kvm-intel"
+    "kvm"
+  ];
+
   virtualisation = {
-    docker = {
-      enable = true;
-      autoPrune.enable = true;
+    docker.enable = true;
+    virtualbox.host.enable = true;
+    virtualbox.host.enableExtensionPack = true;
+
+    libvirtd.enable = true;
+    libvirtd.qemu = {
+      package = pkgs.qemu_kvm;
+      swtpm.enable = true;
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
     };
   };
 
-  users.users."${user}".extraGroups = [ "docker" ];
+  users.users."${user}" = {
+    isNormalUser = true;
+    extraGroups = [
+      "docker"
+      "vboxusers"
+      "libvirtd"
+      "kvm"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    virtualbox # VBoxManage, VBoxHeadless, etc.
+    virt-manager # GUI for libvirt + QEMU/KVM
+  ];
 }
