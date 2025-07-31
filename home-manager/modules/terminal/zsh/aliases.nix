67da80e3482d4bca28ccd7fb8510b1935f09566c
Yazan Alqasem <vwhe@proton.me>
2025-07-31 05:30:18 +0300

    chore: update zsh aliases

--- home-manager/modules/terminal/zsh/aliases.nix
+++ home-manager/modules/terminal/zsh/aliases.nix
@@ -3,51 +3,61 @@
 {
   programs.zsh.shellAliases =
     let
-      flakeDir = "~/System";
       editor = "code";
       nixTemplates = "github:vwh/nixos-config/main?dir=devShells#";
     in
     {
+      # Navigation
       ls = "eza";
       ll = "eza -l";
       etree = "eza --tree --level=1 --icons";
-
       cd = "z";
       ci = "zi";
 
+      # Search
       find = "fd";
       grep = "rg";
       hq = "htmlq";
       cat = "bat";
 
+      # Code info
       tk = "tokei";
       tf = "tokei --files";
 
+      # Files
       fs = "fselect";
 
+      # Editors
       v = "${editor}";
       se = "sudoedit";
 
+      # System info
       ff = "fastfetch";
       mf = "microfetch";
       of = "onefetch";
 
+      # General tools
       lg = "lazygit";
       ld = "lazydocker";
 
+      # AI tools
+      gemi = "gemini";
+      open = "opencode";
+
+      # Nix tools
       ns = "nix-shell";
       nd = "nix develop --command zsh";
       nfu = "nix flake update";
       nfs = "nix flake show";
-
       nfi = "nix flake init";
-      nfib = "nix flake init -t '${nixTemplates}bun'";
-      nfid = "nix flake init -t '${nixTemplates}deno'";
-      nfin = "nix flake init -t '${nixTemplates}nodejs'";
-      nfip = "nix flake init -t '${nixTemplates}python-venv'";
-      nfirs = "nix flake init -t '${nixTemplates}rust-stable'";
-      nfirn = "nix flake init -t '${nixTemplates}rust-nightly'";
+      nfib = "nfi -t '${nixTemplates}bun'";
+      nfid = "nfi -t '${nixTemplates}deno'";
+      nfin = "nfi -t '${nixTemplates}nodejs'";
+      nfip = "nfi -t '${nixTemplates}python-venv'";
+      nfirs = "nfi -t '${nixTemplates}rust-stable'";
+      nfirn = "nfi -t '${nixTemplates}rust-nightly'";
 
+      # System
       docker = "sudo docker";
       reboot = "sudo reboot";
       shutdown = "sudo shutdown now";
