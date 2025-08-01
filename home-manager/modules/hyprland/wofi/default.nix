f9730d265255a338737e006851716e2e7224188f
Yazan Alqasem <vwhe@proton.me>
2025-08-01 05:36:56 +0300

    feat: better wofi

--- home-manager/modules/hyprland/wofi/default.nix
+++ home-manager/modules/hyprland/wofi/default.nix
@@ -5,10 +5,41 @@
     enable = true;
 
     settings = {
-      allow_markup = true;
+      # Appearance
       allow_images = true;
-      width = 350;
-      height = 450;
+      image_size = 32;
+      width = 420;
+      height = 520;
+      location = "center";
+      orientation = "vertical";
+      halign = "fill";
+
+      # Behavior
+      show = "drun,run";
+      prompt = "Search";
+      filter_rate = 100;
+      allow_markup = true;
+      no_actions = true;
+      show_all = false;
+      print_command = true;
+      layer = "overlay";
+      insensitive = true;
+
+      # Performance
+      parse_search = true;
+      search = "";
+
+      # Modes
+      modes = "drun,run";
+
+      # Advanced
+      matching = "contains";
+      hide_scroll = true;
+      dynamic_lines = false;
+
+      # Key bindings
+      key_expand = "Tab";
+      key_exit = "Escape";
     };
   };
 
