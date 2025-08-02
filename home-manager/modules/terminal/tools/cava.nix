b23711f6a849d5266c3530c306ffb9edba50a640
Yazan Alqasem <vwhe@proton.me>
2025-08-02 23:53:21 +0300

    feat: gruvbox cava theme

--- home-manager/modules/terminal/tools/cava.nix
+++ home-manager/modules/terminal/tools/cava.nix
@@ -13,14 +13,14 @@
 
       color = {
         gradient = 1;
-        gradient_color_1 = "'#94e2d5'";
-        gradient_color_2 = "'#89dceb'";
-        gradient_color_3 = "'#74c7ec'";
-        gradient_color_4 = "'#89b4fa'";
-        gradient_color_5 = "'#cba6f7'";
-        gradient_color_6 = "'#f5c2e7'";
-        gradient_color_7 = "'#eba0ac'";
-        gradient_color_8 = "'#f38ba8'";
+        gradient_color_1 = "'#ebdbb2'"; # foreground
+        gradient_color_2 = "'#d65d0e'"; # orange
+        gradient_color_3 = "'#fabd2f'"; # yellow
+        gradient_color_4 = "'#8ec07c'"; # green
+        gradient_color_5 = "'#83a598'"; # aqua
+        gradient_color_6 = "'#d3869b'"; # purple
+        gradient_color_7 = "'#fb4934'"; # red
+        gradient_color_8 = "'#fe8019'"; # bright orange
       };
     };
   };
