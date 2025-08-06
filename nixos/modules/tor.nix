5b15aef37f7430ae72b142d8b2fb7faf5f6b6f98
Yazan Alqasem <vwhe@proton.me>
2025-08-06 13:34:10 +0300

    chore: update tor.nix

--- nixos/modules/tor.nix
+++ nixos/modules/tor.nix
@@ -1,8 +1,10 @@
-# Tor service configuration.
+# Tor service configuration for privacy and anonymity.
+
+{ lib, ... }:
 
 {
   services.tor = {
-    enable = true;
+    enable = lib.mkDefault true;
     torsocks.enable = true;
     client.enable = true;
   };
